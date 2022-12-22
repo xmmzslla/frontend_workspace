397f3169c1479a2b10aa148e84ec745521f98062
xmmzslla <wppq20011206@gmail.com>
2022-12-22 14:51:47 +0900

    Add javascript

---
+++ javascriptdemo/js/b.js
@@ -0,0 +1,7 @@
+// window.onload = function () {
+//   let wrap = document.getElementById('wrap');
+//   console.log(wrap.innerText);
+// };
+
+let wrap = document.getElementById('wrap');
+console.log(wrap.innerText);
