cb426239dd53a5f304f6f89e35d47cdcecc82f02
xmmzslla <wppq20011206@gmail.com>
2022-12-27 15:13:48 +0900

    Add js

---
+++ javascriptdemo/js.041_checkbox.js
@@ -0,0 +1,34 @@
+//  js.041_checkbox.js
+
+// 약관 동의
+document.frm.onsubmit = () => {
+  let admin = document.frm.admin;
+  if (!admin.checked) {
+    alert('약관에 동의해야 합니다.');
+    return false;
+  }
+};
+
+// 전체선택
+document.frm.allCheck.onclick = function () {
+  //   console.log(this.checked);
+
+  //   document.frm.subject1.checked = this.checked;
+  //   document.frm.subject2.checked = this.checked;
+  //   document.frm.subject3.checked = this.checked;
+
+  let checkgroup = document.querySelectorAll('.checkgroup');
+  console.log(checkgroup.length); //3
+
+  //   for (let element of checkgroup) {
+  //     element.checked = this.checked;
+  //   }
+
+  //   ES6 Spread operator : NodeList를 Array로 변경
+  let myThis = this;
+  let iArray = [...checkgroup];
+  iArray.map(function (element) {
+    // alert(this); 여기 this는 window
+    element.checked = myThis.checked;
+  });
+};
