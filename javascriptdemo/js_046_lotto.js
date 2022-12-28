8a7889ac649b033fa9b301d3b5e81ab06a5b435d
xmmzslla <wppq20011206@gmail.com>
2022-12-28 15:11:03 +0900

    Add js

--- javascriptdemo/js_046_lotto.js
+++ javascriptdemo/js_046_lotto.js
@@ -2,8 +2,34 @@
 
 // 1.6개의 난수를 구하고 중복 체크
 let lotto = [];
+for (let i = 0; i <= 5; i++) {
+  // 0.0이상 ~1.0미만
+  // let ran = Math.random();
 
-// 2. 정렬
+  // 0.0이상 ~45.0미만
+  let ran = Math.random() * 45;
+  //1이상 ~ 45까지
+  ran = Math.floor(ran) + 1;
+  console.log(ran);
+  lotto.push(ran);
+  for (let j = 0; j < i; j++) {
+    if (lotto[j] == lotto[i]) {
+      lotto.pop();
+      i--;
+      break;
+    }
+  }
+}
+
+// 2. 오름차순 정렬
+lotto.sort((a, b) => {
+  return a - b;
+});
 
 //3. 출력
 // consol.log(lotto);
+let divList = document.querySelectorAll('div.wrap div');
+
+divList.forEach((element, idx) => {
+  element.innerText = lotto[idx];
+});
