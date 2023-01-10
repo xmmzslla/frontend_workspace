a060217fc69888f740a9b4ebdd2c587dcbea6de5
xmmzslla <wppq20011206@gmail.com>
2023-01-10 21:57:48 +0900

    Add jquery

--- my_homepage/js/js_my_homepage.js
+++ my_homepage/js/js_my_homepage.js
@@ -2,36 +2,36 @@
 
 // 슬라이드
 setInterval(function () {
-  let tg = document.querySelector('.slide');
-  tg.style.top = '-100%';
-  tg.style.transition = '0.8s';
+  let tg = document.querySelector(".slide");
+  tg.style.top = "-100%";
+  tg.style.transition = "0.8s";
 
   setTimeout(function () {
-    let first = tg.querySelectorAll('li')[0];
+    let first = tg.querySelectorAll("li")[0];
     tg.appendChild(first);
-    tg.style.top = '0';
-    tg.style.transition = 'none';
+    tg.style.top = "0";
+    tg.style.transition = "none";
   }, 800);
 }, 4000);
 
 // 탭변경
 function chgTab(i1, i2) {
   // 탭메뉴
-  let tm = document.querySelectorAll('.tm>li');
+  let tm = document.querySelectorAll(".tm>li");
 
   //탭내용
-  let tc = document.querySelectorAll('.tc>li');
+  let tc = document.querySelectorAll(".tc>li");
 
   //   클래스 추가
-  tm[i1].classList.add('on'); //갤러리에 추가
-  tc[i1].classList.add('on');
+  tm[i1].classList.add("on"); //갤러리에 추가
+  tc[i1].classList.add("on");
 
   //   클래스 제거
-  tm[i2].classList.remove('on');
-  tc[i2].classList.remove('on');
+  tm[i2].classList.remove("on");
+  tc[i2].classList.remove("on");
 }
 
 function popup(sts) {
-  var pop = document.querySelector('.pop');
+  var pop = document.querySelector(".pop");
   pop.style.display = sts;
 }
