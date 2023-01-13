fab9c078bef6694d123d09fab2bcc4ac9877af2a
xmmzslla <wppq20011206@gmail.com>
2023-01-13 15:06:35 +0900

    Add html

---
+++ webdemo/WebContent/servletview/part04/insert.jsp
@@ -0,0 +1,31 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>insert</title>
+</head>
+<body>
+<form action="meminsert" method="post">
+<table>
+<tr>
+<th>name</th><td><input type="text" name="name"></td>
+</tr>
+
+<tr>
+<th>age</th><td><input type="text" name="age"></td>
+</tr>
+
+<tr>
+<th>loc</th><td><input type="text" name="loc"></td>
+</tr>
+
+<tr>
+<td colspan="2"><input type="submit" value="commit"></td>
+</tr>
+
+</table>
+</form>
+</body>
+</html>
---
+++ webdemo/src/servletdemo/part04/InsertMemController.java
@@ -0,0 +1,44 @@
+package servletdemo.part04;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//http://localhost:8090/webdemo/meminsert
+
+@WebServlet("/meminsert")
+public class InsertMemController extends HttpServlet{
+	private MemService service;
+	
+	public InsertMemController() {
+	service = new MemService();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String path = "/servletview/part04/insert.jsp";
+		RequestDispatcher dis = req.getRequestDispatcher(path);
+		dis.forward(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("name");
+		int age = Integer.parseInt(req.getParameter("age"));
+		String loc = req.getParameter("loc");
+		MemDTO dto = new MemDTO(name, age, loc);
+		int chk = service.getInsert(dto);
+		
+		if(chk > 0) {
+			resp.sendRedirect("memlist"); //서버에서 클라이언트에 memlist를 요청하도록 응답
+		}else {
+			System.out.println("삽입 실패");
+		}
+	}
+
+}
